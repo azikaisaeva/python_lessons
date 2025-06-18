411a77e52753f3bd09ea294a296bd5ae6aa9937f
Aziza <isaevaazika@gmail.com>
2025-06-18 14:50:03 +0800

    Initial commit

---
+++ lesson1.py
@@ -0,0 +1,163 @@
+
+# Однострочные комментарии
+
+"""
+Многострочные
+комментарии
+1
+2
+3
+"""
+
+# int - (числа/integer) не изменяемый целочисленный тип данных
+any_name = 1
+# any_name это название переменной
+# 1 это целочисленная переменная (интежер)
+
+# str - (строка/string) не изменяемый строковый тип данных
+djanhongir = "Djahongir"
+aichurok = 'Aichurok in Bali'
+
+# float - (цисла с плавающей точкой) не изменяемый тип данных
+float1 = 12.23
+float2 = 1.33
+
+# boolean - (булевые значения) не изменяемый тип данных
+status = True
+is_archived = False
+
+# операторы сравнения
+# == равно ли
+# != не равно
+# >= больше или равно
+# <= меньше или равно
+# > больше
+# < меньше
+
+# # print(float1 == float2)
+# # print(float1 != float2)
+# # print(float1 > float2)
+# # print(float1 >= float2)
+# print(float1 < float2)
+# # print(float1 <= float2)
+#
+# # int() - приводим тип данных в интежер
+# print(int("5"))
+# # str() - приводим тип данных в строку
+# print(str(5))
+# # float() - приводим тип данных в флоат
+# print(float("5"))
+# # bool() - приводим тип данных в булеан
+# # True == 1
+# # False == 0
+# print(bool(1))
+# # type() - выводим тип данных переменной
+# print(type(str(5)))
+# input() - пользователь вводит данные с консоли
+# данные с консоли имеют тип данных строка
+# len() - получаем длину строки
+# s1 = "Aichurok"
+# print(len(s1))
+
+# aichurok = input("Ввeдите имя: ")
+# print(type(aichurok))
+
+
+# age = int(input("Введите ваш возраст: "))  # Ввод числа
+# print("Ваш возраст:", age)
+# print(type(age))  # <class 'int'>
+
+
+a = 10
+b = 3
+# print(a + b)  # 13 (сложение)
+# print(a - b)  # 7 (вычитание)
+# print(a * b)  # 30 (умножение)
+# print(a // b)  # 3 (целочисленное деление)
+# print(a / b)  # 3 (деление с остатком)
+# print(a % b)  # 1 (остаток от деления)
+# print(a ** b)  # 1000 (возведение в степень)
+#
+# pi = float(input("Введите число с плавающей точкой: "))  # Ввод float
+# print("Вы ввели:", pi)
+# print(type(pi))  # <class 'float'>
+# print(round(pi, 2))  # Округление
+#
+# num_str = "3.5"
+# num_float = float(num_str)
+# print(type(num_float))  # <class 'float'>
+
+
+# name = input("Введите ваше имя: ")  # Ввод строки
+# print("Привет, " + name + "!")
+# print(type(name))  # <class 'str'>
+# # text = """Это многострочный текст в Python"""
+# # print(text)
+# word = "Python"
+# print(word)
+# print(word[0])  # P
+# print(word[-1])  # n
+# print(len(word))  # 6
+# number = 42
+# num_str = str(number)
+# print(type(num_str))  # <class 'str'>
+#
+
+# bool - может иметь только два состояния: True or False
+# is_python_easy = input("Python легкий? (да/нет): ") == "да"
+# print("Вы ответили:", is_python_easy)
+# print(type(is_python_easy))  # <class 'bool'>
+# a = 3
+# b = 10
+# print(a >= b)  # False
+# print(a <= b)  # True
+# print(a == b)  # False
+# print(a != b)  # True
+# print(True and False)  # False
+# print(True or False)  # True
+# print(not True)  # False
+# print(a == b and a > b and a < b)  # False
+# print(a > b or a != b)  # True
+# print(False or True)
+
+
+
+
+# Преобразование типов
+print(float(5))  # 5.0
+print(int(5.9))  # 5
+print(int("100"))  # 100
+print(str(42))  # "42"
+print(int(True))  # 1
+print(int(False))  # 0
+
+
+
+# print()  - вывод в консоль, принимает на вход переменные можно через запятую
+# input()  - ввод с консоли, принимает на вход текст который выведет в консоль для ввода
+# type() - выводит тип данных переменной, принимает на вход переменную
+# len() - выводит длину объекта, принимает на вход переменную
+# int() - приведение к типу integer
+# str() - приведение к типу string
+# float() - приведение к типу float
+# lower() - метод строки переводящий всю строку в нижний регистр
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
